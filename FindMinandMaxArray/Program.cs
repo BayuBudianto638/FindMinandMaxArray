1354a85513570d7e7c74bb5dcbf5d52cc72a7290
Bayu <radamanthys_ok@yahoo.com>
2022-07-30 15:06:31 +0700

    Add project files.

---
+++ FindMinandMaxArray/Program.cs
@@ -0,0 +1,8 @@
+﻿var arr = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+var resultMin = arr.Min();
+var resultMax = arr.Max();
+
+Console.WriteLine($"Result Min {resultMin}");
+Console.WriteLine($"Result Max {resultMax}");
+Console.ReadKey();
